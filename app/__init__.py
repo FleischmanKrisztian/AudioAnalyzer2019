e02a1924a692201e1b60d0f54f15713267745af4
FleischmanKrisztian <49558830+FleischmanKrisztian@users.noreply.github.com>
2021-03-07 11:18:59 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 application = Flask(__name__)
 application.config.from_object("config.ProductionConfig")
-# application.run(host= '0.0.0.0', port='5000')
+application.run(host= '0.0.0.0', port='5000')
 
 from app import views
 from app import Models
