0cf2cf03344249f88b6b4d55b902f8c955ab793f
FleischmanKrisztian <49558830+FleischmanKrisztian@users.noreply.github.com>
2021-03-07 23:29:02 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,9 @@ import os
 
 application = Flask(__name__)
 application.config.from_object("config.ProductionConfig")
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    application.run(host='0.0.0.0', port=port)
+# Bind to PORT if defined, otherwise default to 5000.
+port = int(os.environ.get('PORT', 5000))
+application.run(host='0.0.0.0', port=port)
 
 from app import views
 from app import Models
