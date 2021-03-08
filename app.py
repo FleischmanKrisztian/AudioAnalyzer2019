b62151b3b39afcf539c8f1212e50a3013776da4f
FleischmanKrisztian <49558830+FleischmanKrisztian@users.noreply.github.com>
2021-03-08 16:09:26 +0200

    added client mkdirs

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from app import application
 import os
 
 if __name__ == "__main__":
+    print(os.getcwd())
+    # Creating the directories for the deployed app -- should also delete these dirs sometimes
+    os.mkdir('../Flasklast/app/static/client')
+    os.mkdir('../Flasklast/app/static/client/audiofiles')
+    os.mkdir('../Flasklast/app/static/client/images')
+    os.mkdir('../Flasklast/app/static/client/IncomingAudio')
+
     port = int(os.environ.get('PORT', 5000))
     application.run(host='0.0.0.0', port=port)
 
